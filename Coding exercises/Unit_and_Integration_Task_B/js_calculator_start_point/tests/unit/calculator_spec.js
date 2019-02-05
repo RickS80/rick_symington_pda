c1918e4c1e8a03728296d865721758f5c4aed400
Rick Symington <rick.symington@gmail.com>
2019-02-05 15:14:43 +0000

    Add unit tests

--- Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,29 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
-});
+  it('should be able to add', function(){
+    calculator.previousTotal = 4
+    calculator.add(1)
+    assert.strictEqual(5, calculator.runningTotal)
+  });
+
+  it('should be able to subtract', function(){
+    calculator.previousTotal = 7
+    calculator.subtract(4)
+    assert.strictEqual(3, calculator.runningTotal)
+  });
+
+  it('should be able to multiply', function(){
+    calculator.previousTotal = 3
+    calculator.multiply(5)
+    assert.strictEqual(15, calculator.runningTotal)
+  });
+
+  it('should be able to divide', function(){
+    calculator.previousTotal = 21
+    calculator.divide(3)
+    assert.strictEqual(7, calculator.runningTotal)
+  });
+
+
+})
