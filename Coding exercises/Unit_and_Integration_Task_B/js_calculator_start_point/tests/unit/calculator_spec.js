45f5d7805e3f2e63e5f8d793fec15bd7a24c4b03
Rick Symington <rick.symington@gmail.com>
2019-02-05 15:44:26 +0000

    Add integration test

--- Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
+++ Coding exercises/Unit_and_Integration_Task_B/js_calculator_start_point/tests/unit/calculator_spec.js
@@ -35,5 +35,36 @@ describe('calculator', function () {
     assert.strictEqual(7, calculator.runningTotal)
   });
 
+  it('should be able to concatenate mulitple clicks', function(){
+    calculator.numberClick(4)
+    calculator.numberClick(3)
+    assert.strictEqual(43, calculator.runningTotal)
+  });
+
+  it('should be able to chain mulitple operations together', function(){
+    calculator.numberClick(4)
+    calculator.operatorClick('+')
+    calculator.numberClick(6)
+    calculator.operatorClick('/')
+    calculator.numberClick(2)
+    calculator.operatorClick('=')
+    assert.strictEqual(5, calculator.runningTotal)
+  });
+
+  it('should clear the running total without affecting the calculation', function(){
+    calculator.numberClick(10)
+    calculator.operatorClick('+')
+    calculator.numberClick(5)
+    calculator.clearClick()
+    calculator.numberClick(3)
+    calculator.operatorClick('=')
+    assert.strictEqual(13, calculator.runningTotal)
+  });
+
+
+
+
+
+
 
 })
